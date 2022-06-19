c72d38c5bd7d462cb8451bd44bab34b74d0cd125
Franklyn Brea <franklynbrea38@gmail.com>
2022-06-19 15:05:11 -0400

    First commit

---
+++ Emarket.Application/ViewModels/Ad/SaveAdsVM.cs
@@ -0,0 +1,32 @@
+﻿using Emarket.Core.Application.ViewModels.Categories;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emarket.Core.Application.ViewModels.Ad
+{
+    public class SaveAdsVM
+    {
+
+        public int Id { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Name { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Description { get; set; }
+        public List<string> Urls { get; set; }
+        [Range(1, double.MaxValue, ErrorMessage = "Este campo es obligatorio")]
+        public double Price { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Este campo es obligatorio")]
+        public int CategoryId { get; set; }
+        public List<CategoryVM> Categories { get; set; }
+        [DataType(DataType.Upload, ErrorMessage = "Este campo es obligatorio")]
+        public List<IFormFile> File{ get; set; }
+    }
+}
---
+++ Emarket.Application/ViewModels/User/SaveUserVM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emarket.Core.Application.ViewModels.User
+{
+    public class SaveUserVM
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password),ErrorMessage = "Ambas contraseña deben coincidir")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DataType(DataType.Text)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        public bool validateUser { get; set; } = false;
+    }
+}
